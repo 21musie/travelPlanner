5f9464137925800bc01cd503466c7058ef3d90ab
21miusie <mmengesha27@gmail.com>
2025-02-04 23:30:52 +0300

    add options for select traveller

---
+++ constants/Locations.js
@@ -0,0 +1,11 @@
+function Location(id, title, location, category, rating, description, imageUrls, createdAt, author) {
+    this.id = id;
+    this.title = title;
+    this.location = location;
+    this.category = category;
+    this.rating = rating;
+    this.description = description;
+    this.imageUrls = imageUrls;
+    this.createdAt = createdAt;
+    this.author = author;
+}
