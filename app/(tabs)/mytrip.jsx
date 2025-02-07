4cc104556a9454652c9f322a1a1247c87243a352
21miusie <mmengesha27@gmail.com>
2025-02-07 23:36:28 +0300

    start/add new trip--updated UI

--- app/(tabs)/mytrip.jsx
+++ app/(tabs)/mytrip.jsx
@@ -1,95 +1,38 @@
-import {
-    View,
-    Text,
-    ActivityIndicator,
-    TouchableOpacity,
-    ScrollView,
-    Image,
-  } from 'react-native';
-  import React, { useEffect, useState } from 'react';
-  import { Colors } from '@/constants/Colors';
-  import Ionicons from '@expo/vector-icons/Ionicons';
-  import StartNewTripCard from '@/components/MyTrips/StartNewTripCard';
-  import { useRouter } from 'expo-router';
-  import { auth, db } from '@/configs/FirebaseConfig';
-  import { collection, getDocs, query, where } from 'firebase/firestore';
-  import UserTripList from '@/components/MyTrips/UserTripList';
-  
-  export default function MyTrip() {
-    const router = useRouter();
+import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { FontAwesome6 } from '@expo/vector-icons';
+import StartNewTripCard from '@/components/StartNewTripCard';
+import { Colors } from '@/constants/Colors';
+
+export default function MyTrip() {
     const [userTrips, setUserTrips] = useState([]);
-    const [loading, setLoading] = useState(false);
-    const user = auth.currentUser;
-  
-    useEffect(() => {
-      if (user) {
-        GetMyTrips();
-      }
-    }, [user]);
-  
-    const GetMyTrips = async () => {
-      setLoading(true);
-      setUserTrips([]);
-      const q = query(
-        collection(db, 'UserTrips'),
-        where('userEmail', '==', user?.email)
-      );
-  
-      const querySnapshot = await getDocs(q);
-      querySnapshot.forEach((doc) => {
-        const tripData = doc.data(); // No type casting needed
-        setUserTrips((prev) => [...prev, { ...tripData, docId: doc.id }]);
-      });
-      setLoading(false);
-    };
-  
+
     return (
-      <View
-        style={{
-          flex: 1,
-          backgroundColor: Colors.WHITE,
-          padding: 16,
-        }}
-      >
-        <ScrollView
-          showsHorizontalScrollIndicator={false}
-          showsVerticalScrollIndicator={false}
-        >
-          <View
-            style={{
-              display: 'flex',
-              flexDirection: 'row',
-              alignContent: 'center',
-              alignItems: 'center',
-              justifyContent: 'space-between',
-            }}
-          >
-            <Image
-              source={require('@/assets/images/icon.png')}
-              style={{ width: 52, height: 52 }}
-            />
-            <Text
-              style={{
-                fontFamily: 'roboto-bold',
-                fontSize: 33,
-              }}
-            >
-              My Trips
-            </Text>
-            <TouchableOpacity
-              onPress={() => router.push('/create-trip/search-place')}
-            >
-              <Ionicons name="add-circle" size={50} color="black" />
-            </TouchableOpacity>
-          </View>
-  
-          {loading && <ActivityIndicator size={'large'} color={Colors.PRIMARY} />}
-          {userTrips.length === 0 ? (
-            <StartNewTripCard />
-          ) : (
-            <UserTripList userTrips={userTrips} />
-          )}
-        </ScrollView>
-      </View>
+        <View style={styles.page}>
+            <View style={styles.header}>
+                <Text style={styles.headerText}>MyTrip</Text>
+                <FontAwesome6 name="circle-plus" size={35} color="black" />
+            </View>
+
+            {userTrips.length === 0 && <StartNewTripCard />}
+        </View>
     );
-  }
+}
+
+const styles = StyleSheet.create({
+    page: {
+        padding: 25,
+        paddingTop: 55,
+        flex: 1,
+        backgroundColor: Colors.WHITE,
+    },
+    header: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+    },
+    headerText: {
+        fontSize: 35,
+        fontFamily: 'outfit-bold',
+    },
+});
