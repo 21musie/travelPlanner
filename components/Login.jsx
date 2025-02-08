f8c05d253da7b8ec835327c4ed4d5d9e02f7a680
21miusie <mmengesha27@gmail.com>
2025-02-08 23:17:47 +0300

    landing page final

--- components/Login.jsx
+++ components/Login.jsx
@@ -1,81 +1,106 @@
-import { Colors } from '@/constants/Colors';
-import { useRouter } from 'expo-router';
-import React from 'react';
-import { Image, StyleSheet, Text, View, Dimensions } from 'react-native';
-import { TouchableOpacity } from 'react-native';
+import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native'
+import React from 'react'
+import { Colors } from '@/constants/Colors'
+import { useRouter } from 'expo-router'
 
 export default function Login() {
-  const router = useRouter();
-  const { height } = Dimensions.get('window');
 
-  return (
-    <View>
-      <Image
-        source={require('./../assets/images/login.png')}
-        style={{
-          width: '100%',
-          height: height * 0.4,
-        }}
-      />
-      <View style={styles.container}>
-        <Text
-          style={{
-            fontSize: 30,
-            fontFamily: 'roboto-bold',
-            textAlign: 'center',
-            marginTop: 10,
-          }}
-        >
-          AI Travel Planner
-        </Text>
+    const router = useRouter();
 
-        <Text
-          style={{
-            fontFamily: 'roboto-regular',
-            fontSize: 17,
-            textAlign: 'center',
-            color: Colors.GRAY,
-            marginTop: 20,
-          }}
-        >
-          Discover your next adventure effortlessly. Personalized itineraries at
-          your fingertips. Travel smarter with AI-driven insights.
-        </Text>
-
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => router.push('/auth/sign-in')}
-        >
-          <Text
-            style={{
-              color: Colors.WHITE,
-              textAlign: 'center',
-              fontFamily: 'roboto-regular',
-              fontSize: 17,
-            }}
-          >
-            Get started
-          </Text>
-        </TouchableOpacity>
-      </View>
-    </View>
-  );
+    return (
+        <View style={styles.mainContainer}>
+            <View style={styles.imageContainer}>
+                <Image source={require('./../assets/images/login.png')}
+                    style={styles.image}
+                />
+            </View>
+            <View style={styles.container}>
+                <View style={styles.heading}>
+                    <Text style={styles.title}> Travel🌍Planner</Text>
+                    <Text style={styles.subtitle}>Your AI integrated Travel Planner 🤖</Text>
+                </View>
+                <Text style={styles.description}>
+                    Explore the world effortlessly with our AI-powered travel planner. Get personalized travel plans and recommendations tailored just for you.
+                </Text>
+                <TouchableOpacity
+                    onPress={() => router.push('/auth/sign-in')}
+                    style={styles.button}>
+                    <Text style={styles.buttonText}>
+                        Get Started
+                    </Text>
+                </TouchableOpacity>
+                <View style={styles.spacer} />
+                <View>
+                    <Text style={styles.footerText}>
+                       
+                    </Text>
+                </View>
+            </View>
+        </View>
+    )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: Colors.WHITE,
-    marginTop: -20,
-    borderTopLeftRadius: 30,
-    borderTopRightRadius: 30,
-    padding: 25,
-    height: '100%',
-  },
 
-  button: {
-    padding: 15,
-    backgroundColor: Colors.PRIMARY,
-    borderRadius: 99,
-    marginTop: '20%',
-  },
-});
+const styles = StyleSheet.create({
+    mainContainer: {
+        flex: 1,
+        backgroundColor: Colors.WHITE,
+    },
+    imageContainer: {
+        paddingTop: '10%',
+        backgroundColor: '#069494',
+    },
+    image: {
+        width: '100%',
+        height: 480,
+    },
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        backgroundColor: Colors.WHITE,
+        marginTop: '-10%',
+        padding: 20,
+        borderTopLeftRadius: 30,
+        borderTopRightRadius: 30,
+        gap: 10,
+        justifyContent: 'space-between',
+    },
+    heading: {
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    title: {
+        fontSize: 28,
+        fontFamily: 'outfit-bold',
+    },
+    subtitle: {
+        fontSize: 14,
+        fontFamily: 'outfit-medium',
+    },
+    description: {
+        fontSize: 17,
+        fontFamily: 'outfit',
+        color: Colors.GRAY,
+        textAlign: 'center',
+    },
+    button: {
+        backgroundColor: Colors.BLACK,
+        padding: 15,
+        borderRadius: 15,
+        marginTop: 20,
+        width: '80%',
+        alignItems: 'center',
+        elevation: 5,
+    },
+    buttonText: {
+        fontSize: 17,
+        fontFamily: 'outfit',
+        color: Colors.WHITE,
+    },
+    spacer: {
+        flex: 1,
+    },
+    footerText: {
+        textAlign: 'center',
+    }
+})
